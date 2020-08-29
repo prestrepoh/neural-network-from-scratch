c0e72e45ebf472b9d01b41609d7a9834741e3745
Pablo Restrepo <prestrepoh>
2020-08-29 16:28:52 +0200

    added nn code

---
+++ src/networks/layer.py
@@ -0,0 +1,85 @@
+import numpy as np
+
+
+def affine_forward(x, w, b):
+    """
+    Computes the forward pass for an affine (fully-connected) layer.
+
+    The input x has shape (N, d_1, ..., d_k) and contains a minibatch of N
+    examples, where each example x[i] has shape (d_1, ..., d_k). We will
+    reshape each input into a vector of dimension D = d_1 * ... * d_k, and
+    then transform it to an output vector of dimension M.
+
+    Inputs:
+    :param x: A numpy array containing input data, of shape (N, d_1, ..., d_k)
+    :param w: A numpy array of weights, of shape (D, M)
+    :param b: A numpy array of biases, of shape (M,)
+
+    :return out: output, of shape (N, M)
+    :return cache: (x, w, b)
+    """
+
+    reshaped = x
+    reshaped = reshaped.reshape(x.shape[0], w.shape[0])
+
+    out = (np.dot(reshaped, w)) + b
+
+    cache = (x, w, b)
+    return out, cache
+
+
+def affine_backward(dout, cache):
+    """
+    Computes the backward pass for an affine layer.
+
+    Inputs:
+    :param dout: Upstream derivative, of shape (N, M)
+    :param cache: Tuple of:
+      - x: Input data, of shape (N, d_1, ... d_k)
+      - w: Weights, of shape (D, M)
+      - b: A numpy array of biases, of shape (M,
+
+    :return dx: Gradient with respect to x, of shape (N, d1, ..., d_k)
+    :return dw: Gradient with respect to w, of shape (D, M)
+    :return db: Gradient with respect to b, of shape (M,)
+    """
+    x, w, b = cache
+
+    dx = dout.dot(w.T).reshape(x.shape)
+    dw = (x.reshape(x.shape[0], w.shape[0]).T.dot(dout))/x.shape[0]
+    db = (np.sum(dout, axis=0))/x.shape[0]
+
+    return dx, dw, db
+
+
+def sigmoid_forward(x):
+    """
+    Computes the forward pass for a layer of sigmoids.
+
+    :param x: Inputs, of any shape
+
+    :return out: Output, of the same shape as x
+    :return cache: out
+    """
+
+    out = out = 1 / (1 + np.exp(-x))
+
+    cache = out
+    return out, cache
+
+
+def sigmoid_backward(dout, cache):
+    """
+    Computes the backward pass for a layer of sigmoids.
+
+    :param dout: Upstream derivatives, of any shape
+    :param cache: y, output of the forward pass, of same shape as dout
+
+    :return dx: Gradient with respect to x
+    """
+    dx = None
+    y = cache
+
+    dx = (y * (1 - y)) * dout
+
+    return dx
